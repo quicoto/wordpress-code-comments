2d8d90aa3a3b5b1603051dfa51bd1764b7aacb15
Ricard Torres <ricard.torres@netcentric.biz>
2019-11-27 10:44:06 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/*
+
+	Plugin Name: WordPress Code Comments
+	Plugin URI: https://php.quicoto.com
+	Description: A WordPress plugin which escapes the HTML inside the <code> blocks in comments
+	Author: Ricard Torres
+	Version: 1.0
+	Author URI: https://ricard.blog
+*/
+
+function encode_code_in_comment( $comment ) {
+	$encoded = preg_replace_callback( '/<code>(.*?)<\/code>/ims',
+		create_function(
+			'$matches',
+			'$matches[1] = preg_replace(
+			array("/^[\r|\n]+/i", "/[\r|\n]+$/i"), "",
+			$matches[1]);
+			return "<code>" . htmlspecialchars($matches[1]) . "</code>";'
+		),
+		$source
+  );
+
+
+  if ($encoded) return $encoded;
+
+  return $comment
+}
+
+add_filter( 'comment_text', 'encode_code_in_comment', '', 1);
+
