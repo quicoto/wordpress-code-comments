776fce76e8356750b58db843e06b8d8cb03ce7b4
Ricard Torres <ricard.torres@netcentric.biz>
2019-11-27 10:50:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,16 +11,11 @@
 
 function encode_code_in_comment( $comment ) {
 	$encoded = preg_replace_callback( '/<code>(.*?)<\/code>/ims',
-		create_function(
-			'$matches',
-			'$matches[1] = preg_replace(
-			array("/^[\r|\n]+/i", "/[\r|\n]+$/i"), "",
-			$matches[1]);
-			return "<code>" . htmlspecialchars($matches[1]) . "</code>";'
-		),
-		$source
+		function ($matches) {
+      return '<code>' . htmlspecialchars($matches[1]) . '</code>';
+    },
+		$comment
   );
-
 
   if ($encoded) return $encoded;
 
